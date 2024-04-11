b9c7b6d9434d00dab7e6f02428f42a54d32f5ea3
Andres Felipe Murcia Zapata <andresf.murciaz@gmail.com>
2024-04-11 14:40:59 -0500

    my orders

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,12 +1,28 @@
 import Layout from "../../Components/Layout"
+import { useContext } from "react"
+import OrdersCard from '../../Components/OrdersCard'
+import { ShoppingCartContext } from "../../Context"
+import { Link } from 'react-router-dom'
 
 function MyOrders() {
- return( 
-    <Layout>
-    hola my orders
-        </Layout>
- )
+    const context = useContext(ShoppingCartContext)
   
-}
+    return (
+      <Layout>
+        <div className='flex items-center justify-center relative w-80'>
+          <h1>My Orders</h1>
+        </div>
+        {
+          context.order.map((order, index) => {
+            <Link key={index} to={`/my-orders/${order.id}`}>
+              <OrdersCard
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts} />
+            </Link>
+          })
+        }
+      </Layout>
+    )
+  }
 
 export default MyOrders
